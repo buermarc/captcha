8807c0699042feb3797e3db3ef8c9fc82f767ab5
Nicolas Bender <nicolas.bender@sap.com>
2022-09-14 23:51:20 +0200

    Added modified captcha and default rcnn model

--- generate_captcha.py
+++ generate_captcha.py
@@ -1,18 +1,15 @@
-from captcha.image import ImageCaptcha
+from pythoncaptcha.image import ImageCaptcha
 import random, string
 
 image = ImageCaptcha()
 
-def generate_python_captcha(number_of_chars: int = 0):
+def generate_python_captcha(number_of_chars: int = 0, boxes: bool = False):
     if(number_of_chars == 0):
         number_of_chars = random.randrange(2)+4
     content = "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(number_of_chars)) # take uppercase only to reduce similar chars like w and W
     #data = image.generate(content)
-    image.write(content, f"generated_captchas/imagecaptcha/{content}.png")
-
-def generate_own_captcha():
-    pass
+    print(image.write(content, f"generated_captchas/pythoncaptcha/{content}.png", boxes=boxes))
 
 if __name__ == "__main__":
-    for i in range(10):
-        generate_python_captcha()
+    for i in range(5):
+        generate_python_captcha(boxes=False)
---
+++ rcnn-resnet50.py
@@ -0,0 +1,23 @@
+import torch, torchvision
+from torchvision.models.detection import FasterRCNN_ResNet50_FPN_Weights
+
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn()
+# For training
+images, boxes = torch.rand(4, 3, 600, 1200), torch.rand(4, 11, 4)
+boxes[:, :, 2:4] = boxes[:, :, 0:2] + boxes[:, :, 2:4]
+labels = torch.randint(1, 91, (4, 11))
+images = list(image for image in images)
+targets = []
+for i in range(len(images)):
+    d = {}
+    d['boxes'] = boxes[i]
+    d['labels'] = labels[i]
+    targets.append(d)
+output = model(images, targets)
+# For inference
+model.eval()
+x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
+predictions = model(x)
+
+# optionally, if you want to export the model to ONNX:
+torch.onnx.export(model, x, "faster_rcnn.onnx", opset_version = 11)
