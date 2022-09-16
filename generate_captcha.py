4aae89bcb5bfc760b4c394e2ff762a3b128c7b2d
buermarc <buermarc2@gmail.com>
2022-09-16 17:28:49 +0200

    Auto generate outdir and formating

--- generate_captcha.py
+++ generate_captcha.py
@@ -2,30 +2,47 @@ from pythoncaptcha.image import ImageCaptcha
 import argparse
 import random, string
 import json
+import os
 
 image = ImageCaptcha()
+OUTDIR = "generated_captchas"
 
-def generate_python_captcha(labels: dict, number_of_chars: int = 0, boxes: bool = False):
-    if(number_of_chars == 0):
-        number_of_chars = random.randrange(2)+4
-    content = "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(number_of_chars)) # take uppercase only to reduce similar chars like w and W
-    #data = image.generate(content)
 
-    offset_points = image.write(
-        content,
-        f"generated_captchas/{content}.png", boxes=boxes
-    )
+def generate_python_captcha(
+    labels: dict, number_of_chars: int = 0, boxes: bool = False
+):
+    if number_of_chars == 0:
+        number_of_chars = random.randrange(2) + 4
+    content = "".join(
+        random.choice(string.ascii_uppercase + string.digits)
+        for _ in range(number_of_chars)
+    )  # take uppercase only to reduce similar chars like w and W
 
-    labels[content] = {"boxes": [[x1, y1, x2, y2] for (x1, y1), (x2, y2) in offset_points], "labels": [*content]}
+    offset_points = image.write(content, f"{OUTDIR}/{content}.png", boxes=boxes)
+
+    labels[content] = {
+        "boxes": [[x1, y1, x2, y2] for (x1, y1), (x2, y2) in offset_points],
+        "labels": [*content],
+    }
 
     return labels
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Generate catpchas')
-    parser.add_argument('amount', metavar='N', type=int, nargs='?', help='Amount of to be generated captchas', default=6)
+    parser = argparse.ArgumentParser(description="Generate catpchas")
+    parser.add_argument(
+        "amount",
+        metavar="N",
+        type=int,
+        nargs="?",
+        help="Amount of to be generated captchas",
+        default=6,
+    )
     args = parser.parse_args()
+
+    if not os.path.exists(OUTDIR):
+        os.mkdir(OUTDIR)
 
     labels = {}
     for i in range(args.amount):
