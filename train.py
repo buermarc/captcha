fb60b3f299d34a0ff0664d9b581d43837c14043c
buermarc <buermarc2@gmail.com>
2022-09-16 17:33:12 +0200

    train: auto assign worker cores and gpu or cpu

--- train.py
+++ train.py
@@ -1,25 +1,34 @@
 from pathlib import Path
+import os
+import torch
 from torch.utils.data import DataLoader
 from captcha_dataset import CaptachDataset
 import pytorch_lightning as pl
-import torchvision.transforms
 from custom_rcnn_lightning_model import CustomRcnnLightningModel
-
-train_data = CaptachDataset(
-    image_path=Path("generated_captchas"),
-    label_file=Path("labels.json")
-)
 
 
 def collate_fn(batch):
     return tuple(zip(*batch))
 
-train_dataloader = DataLoader(train_data, batch_size=64, shuffle=True, collate_fn=collate_fn)
+
+train_data = CaptachDataset(
+    image_path=Path("generated_captchas"), label_file=Path("labels.json")
+)
+
+
+core_count = os.cpu_count()
+train_dataloader = DataLoader(
+    train_data,
+    batch_size=2,
+    shuffle=True,
+    collate_fn=collate_fn,
+    num_workers=int(core_count / 2) if core_count else 4,
+)
 
 model = CustomRcnnLightningModel()
 
 trainer = pl.Trainer(
-    gpus=0,
+    gpus=1 if torch.cuda.is_available() else 0,
     max_epochs=2,
 )
 
