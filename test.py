021f289b317877c55262a5f5362608397f819182
Nicolas Bender <nicolas.bender@sap.com>
2022-09-17 17:17:52 +0200

    merged

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import torchvision
 import utils
+import torchvision, os, utils
 from PIL import Image
 from custom_rcnn_lightning_model import CustomRcnnLightningModel
 
-model = CustomRcnnLightningModel.load_from_checkpoint("tb_logs/CustomRcnnLightningModel/version_2/checkpoints/epoch=2-step=210.ckpt")
 
-testimage = Image.open("data/test/YQ2WV.png")
+version = 2
+checkpoint_file = os.lsitdir("tb_logs/CustomRcnnLightningModel/version_{version}/checkpoints/")[0]
+
+model = CustomRcnnLightningModel.load_from_checkpoint(checkpoint_file)
+
+testimage_file = os.listdir("data/test/")[0]
+testimage = Image.open(testimage_file)
 testtensor = [torchvision.transforms.ToTensor()(testimage)]
 
 output = model.forward(testtensor)
