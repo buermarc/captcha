70ec1ea0bfba3aab3f068e3a93c13050affbe7a4
buermarc <buermarc2@gmail.com>
2022-09-18 12:26:06 +0200

    Test all images in data/test

--- test.py
+++ test.py
@@ -5,15 +5,15 @@ from PIL import Image
 from custom_rcnn_lightning_model import CustomRcnnLightningModel
 
 
-version = 2
+version = 14
 checkpoint_file = os.listdir(f"./tb_logs/CustomRcnnLightningModel/version_{version}/checkpoints/")[0]
 
 model = CustomRcnnLightningModel.load_from_checkpoint(f"./tb_logs/CustomRcnnLightningModel/version_{version}/checkpoints/{checkpoint_file}")
 
-testimage_file = os.listdir(f"./data/test/")[0]
-testimage = Image.open(f"./data/test/{testimage_file}")
-testtensor = [torchvision.transforms.ToTensor()(testimage)]
+for testimage_file in os.listdir("./data/test/"):
+    testimage = Image.open(f"./data/test/{testimage_file}")
+    testtensor = [torchvision.transforms.ToTensor()(testimage)]
 
-output = model.forward(testtensor)
+    output = model.forward(testtensor)
 
-utils.show_dataset(testimage, output[0], threshold=0.1)
+    utils.show_dataset(testimage, output[0], threshold=0.1)
