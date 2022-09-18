8afd10aea27eac36a725517f078d391f618091df
Nicolas Bender <nicolas.bender@sap.com>
2022-09-18 17:00:11 +0200

    Added testmetric

--- test.py
+++ test.py
@@ -10,10 +10,13 @@ checkpoint_file = os.listdir(f"./tb_logs/CustomRcnnLightningModel/version_{versi
 
 model = CustomRcnnLightningModel.load_from_checkpoint(f"./tb_logs/CustomRcnnLightningModel/version_{version}/checkpoints/{checkpoint_file}")
 
+outputs = []
 for testimage_file in os.listdir("./data/test/"):
     testimage = Image.open(f"./data/test/{testimage_file}")
     testtensor = [torchvision.transforms.ToTensor()(testimage)]
 
     output = model.forward(testtensor)
+    outputs.append[output]
+    #utils.show_dataset(testimage, output[0], threshold=0.1)
 
-    utils.show_dataset(testimage, output[0], threshold=0.1)
+utils.own_testmetric(testimage_file.replace(".png", ""), outputs, threshold=0)
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import json, torch
+import numpy as np
 from PIL import ImageFont
 from PIL.ImageDraw import Draw
 
@@ -23,12 +25,14 @@ def show_dataset(image, target, threshold: float = 0.0):
             draw.text(box[0:2], text, font=font, fill='red')
     image.show()
 
-def decode_label(label: int):
-    if isinstance(label, list):
+def decode_label(label):
+    if isinstance(label, list) or isinstance(label, np.ndarray):
         return [_decode_label(element) for element in label]
     return _decode_label(label)
 
-def _decode_label(label: int):
+def _decode_label(label):
+    if not isinstance(label, int):
+        label = int(label)
     if 0 <= label < 62:
         if label < 10:
             return label
@@ -50,3 +54,134 @@ def _encode_label(label: str):
             return ord(label)-55
         return ord(label)-61
     raise "Label has to be alphanumeric"
+
+
+def own_testmetric(correct_labels, datasets, threshold=0):
+    if len(correct_labels) is not len(datasets):
+        raise "Number of correct labels and datasets are not equal"
+
+    correct_detections = 0
+    correct_letters_list = []
+    for correct_label, dataset in zip(correct_labels, datasets):
+        if len(dataset.get('boxes').detach().numpy()) > 0:
+            labels_sorted = _sort_labels_threshold(dataset, threshold)
+            correct_detection = False
+            if len(correct_label) == len(labels_sorted):
+                correct_letters, correct_detection = _check_letters_correct_number(correct_label, labels_sorted)
+            else:
+                correct_letters = _check_letters_wrong_number(correct_label, labels_sorted)
+            correct_letters_list.append(correct_letters)
+            correct_detections += int(correct_detection)
+        else:
+            correct_letters_list.append(0)
+    
+    letters_mean = np.mean(correct_letters_list)
+    letters_std = np.std(correct_letters_list)
+    correct_percentage = correct_detections / len(correct_labels)
+    return correct_percentage, letters_mean, letters_std
+
+
+def _check_letters_wrong_number(correct_label, labels_sorted):
+    if len(labels_sorted) > len(correct_label):
+        x = correct_label
+        y = labels_sorted
+    else:
+        x = labels_sorted
+        y = correct_label
+    
+    correct_letters = 0
+    
+    idy = 0
+    for idx in range(len(x)):
+        delta = 0
+        correct, delta = _recursive_letter_check(x, y, idx, idy, delta)
+        correct_letters += correct
+        idy += 1+delta
+    return correct_letters/len(y)
+
+
+def _recursive_letter_check(x, y, idx, idy, delta):
+    if idy+delta == len(y):
+        return 0, delta
+    else:
+        if x[idx] == y[idy+delta]:
+            return 1, delta
+        else:
+            return _recursive_letter_check(x, y, idx, idy, delta+1)
+
+
+def _check_letters_correct_number(x, y):
+    correct_letters = 0
+    for letter_x, letter_y in zip(x,y):
+        if letter_x == letter_y:
+            correct_letters +=1
+    correct_detection = True if correct_letters == len(x) else False
+    return correct_letters/len(x), correct_detection
+
+
+def _sort_labels_threshold(dataset, threshold):
+    boxes = dataset.get('boxes').detach().numpy()
+    labels = dataset.get('labels').detach().numpy()
+    scores = dataset.get('scores').detach().numpy()
+
+    boxes_upper_left = boxes[:, 0]
+    index_order = np.argsort(boxes_upper_left)
+    labels_sorted = labels[index_order]
+    scores_sorted = scores[index_order]
+
+    labels_concat = ""
+    for score, label in zip(scores_sorted, decode_label(labels_sorted)):
+        if score > threshold:
+            labels_concat += str(label)
+    return labels_concat
+
+
+if __name__ == '__main__':
+    with open("data/train_labels.json", mode="r") as _file:
+        labels_json = json.load(_file)
+    
+    mock = [
+    {
+        'boxes': torch.Tensor(
+            [[2.2495e+01, 2.8099e+00, 4.1562e+01, 5.6435e+01],
+            [1.1614e+02, 2.4039e+00, 1.3830e+02, 5.6009e+01],
+            [5.0569e+01, 8.1115e-02, 7.4713e+01, 5.9538e+01],
+            [8.1298e+01, 1.9613e+00, 1.0305e+02, 5.6535e+01],
+            [9.8708e+01, 0.0000e+00, 1.2088e+02, 5.9347e+01]]),
+        'labels': torch.Tensor([23, 15,  9, 19, 21]),
+        'scores': torch.Tensor([0.9952, 0.9949, 0.9898, 0.9891, 0.9889])
+    },
+    {
+        'boxes': torch.Tensor(
+            [[2.2495e+01, 2.8099e+00, 4.1562e+01, 5.6435e+01],
+            [1.1614e+02, 2.4039e+00, 1.3830e+02, 5.6009e+01],
+            [5.0569e+01, 8.1115e-02, 7.4713e+01, 5.9538e+01],
+            [2.4039e+01, 8.1115e-02, 7.4713e+01, 5.9538e+01],
+            [8.1298e+01, 1.9613e+00, 1.0305e+02, 5.6535e+01],
+            [9.8708e+01, 0.0000e+00, 1.2088e+02, 5.9347e+01]]),
+        'labels': torch.Tensor([23, 15,  9, 7, 19, 21]),
+        'scores': torch.Tensor([0.9952, 0.9949, 0.9898, 0.4, 0.9891, 0.9889])
+    },
+    {
+       'boxes': torch.Tensor(
+            [[2.2495e+01, 2.8099e+00, 4.1562e+01, 5.6435e+01],
+            [1.1614e+02, 2.4039e+00, 1.3830e+02, 5.6009e+01],
+            [8.1298e+01, 1.9613e+00, 1.0305e+02, 5.6535e+01],
+            [9.8708e+01, 0.0000e+00, 1.2088e+02, 5.9347e+01]]),
+        'labels': torch.Tensor([23, 15, 19, 21]),
+        'scores': torch.Tensor([0.9952, 0.9949, 0.9891, 0.9889])
+    },
+    {
+       'boxes': torch.Tensor(
+            [[9.8708e+01, 0.0000e+00, 1.2088e+02, 5.9347e+01]]),
+        'labels': torch.Tensor([21]),
+        'scores': torch.Tensor([0.9889])
+    },
+    {
+       'boxes': torch.Tensor([]),
+        'labels': torch.Tensor([]),
+        'scores': torch.Tensor([])
+    },
+    ]
+
+    print(own_testmetric(["N9JLF", "N9JLF", "N9JLF", "N9JLF", "N9JLF"], mock))
