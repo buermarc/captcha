ff9398870b64a939ee6ab4d3344f9bcf79b5d050
buermarc <buermarc2@gmail.com>
2022-09-20 18:47:49 +0200

    Update test.py to provide epoch wise evaluation

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
+import glob
+import json
 import os
+import re
 from io import BytesIO
 from typing import Dict, Optional
 
@@ -14,11 +17,29 @@ from custom_rcnn_lightning_model import CustomRcnnLightningModel
 pictures: Dict[str, Optional[Image.Image]] = {}
 outputs = []
 
+VERSION = 43
+THRESHOLD = 0.5
+
 
 def index() -> HTMLResponse:
+    _re = re.compile(r".*epoch_(?P<epoch>[0-9]+)_test_output.*-(?P<image>.+\.png)")
+    print(pictures)
     _html = "<html>\n"
-    for key in pictures.keys():
-        _html += f'<img src="/image/{key}" alt="/image/{key}">\n'
+    keys = pictures.keys()
+    epochs = [int(_re.match(key).group("epoch")) for key in keys]
+    epochs.sort()
+    images = set([_re.match(key).group("image") for key in keys])
+    images = [str(image) for image in images]
+    images.sort()
+    _min = min(epochs)
+    _max = max(epochs)
+
+    _html = "<html>"
+    for i in range(_min, _max + 1):
+        for image in images:
+            image_key = f"epoch_{i}_test_output-{image}"
+            _html += f'<img src="/image/{image_key}" alt="/image/{image_key}">\n'
+        _html += "<br>\n"
     _html += "</html>"
 
     return HTMLResponse(_html)
@@ -32,33 +53,30 @@ def serve_image_file(file_name: str) -> StreamingResponse:
     return StreamingResponse(image, media_type="image/png")
 
 
-def load() -> None:
-    version = 21
+def load_v1() -> None:
     checkpoint_file = os.listdir(
-        f"./tb_logs/CustomRcnnLightningModel/version_{version}/checkpoints/"
+        f"./tb_logs/CustomRcnnLightningModel/version_{VERSION}/checkpoints/"
     )[0]
 
     model = CustomRcnnLightningModel.load_from_checkpoint(
-        f"./tb_logs/CustomRcnnLightningModel/version_{version}/checkpoints/{checkpoint_file}"
+        f"./tb_logs/CustomRcnnLightningModel/version_{VERSION}/checkpoints/{checkpoint_file}"
     )
 
     images = {}
-    image_files = os.listdir("./data/test/")
+    image_files = os.listdir("./data/save_test/")
 
     image_tensors = torch.zeros((len(image_files), 3, 60, 160))
     for idx, image in enumerate(image_files):
-        testimage = Image.open(f"./data/test/{image}")
+        testimage = Image.open(f"./data/save_test/{image}")
         images[image] = testimage
         image_tensors[idx] = torchvision.transforms.ToTensor()(testimage)
 
     outputs.extend(model.forward(image_tensors))
 
-    threshold = 0.9
-
     for image_file, output in zip(image_files, outputs):
         image = images[image_file]
         pictures[image_file] = utils.show_dataset(
-            image, output, ret=True, threshold=threshold
+            image, output, ret=True, threshold=THRESHOLD
         )
 
     for output in outputs:
@@ -67,14 +85,48 @@ def load() -> None:
         utils.own_testmetric(
             [image.replace(".png", "") for image in images],
             outputs,
-            threshold=threshold,
+            threshold=THRESHOLD,
         )
     )
 
 
+def load_v2() -> None:
+    # load images from save_test dir
+    # load outputs from current logdir
+    # display images
+
+    images = {}
+    image_files = os.listdir("./data/save_test/")
+
+    image_tensors = torch.zeros((len(image_files), 3, 60, 160))
+    for idx, image in enumerate(image_files):
+        testimage = Image.open(f"./data/save_test/{image}")
+        images[image] = testimage
+        image_tensors[idx] = torchvision.transforms.ToTensor()(testimage)
+
+    output_files = glob.glob(
+        f"./tb_logs/CustomRcnnLightningModel/version_{VERSION}/*.json"
+    )
+
+    output_versions = [
+        json.load(open(output_file, mode="r")) for output_file in output_files
+    ]
+
+    # Don't even try to understand it
+    for outputs, output_file in zip(output_versions, output_files):
+        for output in outputs:
+            image = images[output["name"]].copy()
+            try:
+                pictures[
+                    f"{os.path.basename(output_file).replace('.json','')}-{output['name']}"
+                ] = utils.show_dataset(image, output, ret=True, threshold=THRESHOLD)
+            except Exception as exc:
+                print(exc)
+
+
 def create_app() -> FastAPI:
     app = FastAPI()
-    load()
+    load_v2()
     app.add_api_route("/", index)
     app.add_api_route("/image/{file_name}", serve_image_file)
     return app
