fcd8d6300f792b32731d9065d89f80af1c64311b
buermarc <buermarc2@gmail.com>
2022-09-21 20:46:15 +0200

    isort and black captcha_dataset.py

--- captcha_dataset.py
+++ captcha_dataset.py
@@ -1,15 +1,17 @@
-from pathlib import Path
-from os.path import basename
-import json, utils
-from torch.utils.data import Dataset
-import torch
-from typing import Tuple, Union, Dict, List, Any
-from torchvision.io import read_image
 import glob
+import json
+from os.path import basename
+from pathlib import Path
+from typing import Any, Dict, Tuple
+
+import torch
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+
+import utils
 
 
 class CaptachDataset(Dataset):
-
     def __init__(
         self,
         image_path: Path,
@@ -17,7 +19,7 @@ class CaptachDataset(Dataset):
         use_cache: bool = False,
         transform: Any = None,
         file_ending: str = "png",
-        preferred_datatyp = torch.double,
+        preferred_datatyp=torch.double,
     ):
         self.image_path = image_path
         self.label_file = label_file
@@ -25,9 +27,14 @@ class CaptachDataset(Dataset):
             labels_json = json.load(_file)
             self.labels_json = {
                 content: {
-                    "boxes": torch.Tensor(labels_json[content]["boxes"]).to(torch.int64),
-                    "labels": torch.Tensor(utils.encode_label(labels_json[content]["labels"])).to(torch.int64)
-                } for content in labels_json.keys()
+                    "boxes": torch.Tensor(labels_json[content]["boxes"]).to(
+                        torch.int64
+                    ),
+                    "labels": torch.Tensor(
+                        utils.encode_label(labels_json[content]["labels"])
+                    ).to(torch.int64),
+                }
+                for content in labels_json.keys()
             }
 
         self.use_cache = use_cache
@@ -42,6 +49,7 @@ class CaptachDataset(Dataset):
         if self.use_cache:
             # Use multiprocessing to load images and targets into RAM
             from multiprocessing import Pool
+
             with Pool() as pool:
                 self.cached_data = pool.starmap(self._load_index, iter(self.images))
 
@@ -55,13 +63,10 @@ class CaptachDataset(Dataset):
         image = read_image(self.images[index]).to(self.preferred_datatype) / 255
         content = basename(self.images[index]).replace(f".{self.file_ending}", "")
         labels = self.labels_json[content]
-        
+
         return image, labels
 
-    def __getitem__(
-        self,
-        index: int
-    ) -> Tuple[torch.Tensor, Dict[str, torch.Tensor]]:
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, Dict[str, torch.Tensor]]:
 
         if self.use_cache:
             image, labels = self.cached_data[index]
